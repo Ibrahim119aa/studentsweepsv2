9cfd981f4f7aaf3f066c26041685d07b3b2748bb
Ibrahim <139770979+Ibrahim119aa@users.noreply.github.com>
2025-12-16 00:03:22 -0800

    upload after updating base url

--- socketClient.browser.js
+++ socketClient.browser.js
@@ -1,6 +1,6 @@
 window.SocketClient = (function() {
   // const SOCKET_URL = 'https://api.sweepstackz.com';
-  const SOCKET_URL = 'http://localhost:5053';
+  const SOCKET_URL = 'https://studentsweeps.com/api';
 
   const TOKEN_KEY = 'sweepstackz_token';
   const USER_KEY = 'sweepstackz_user';
